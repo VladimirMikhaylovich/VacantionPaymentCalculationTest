d25b62b5bbe4e7101772e326bc09e9ae59f7a045
Vladimir Mickhaylovich <yalt.vladimir@gmail.com>
2022-10-09 11:07:42 +0300

    Initial commit

---
+++ src/main/java/com/example/vacantionpaymentcalculation/VacantionPaymentCalculationApplication.java
@@ -0,0 +1,13 @@
+package com.example.vacantionpaymentcalculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacantionPaymentCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacantionPaymentCalculationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/vacantionpaymentcalculation/VacantionPaymentCalculationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vacantionpaymentcalculation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacantionPaymentCalculationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
